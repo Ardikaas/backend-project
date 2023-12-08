d2557f0254eeebbc945a53b016d758f94b1ce5b6
Ardikaas <ardikaajisetiawan@gmail.com>
2023-12-08 18:35:43 +0700

    testing respons

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = express()
 
 app.use(express.json())
 
-
+app.get('/', async(req,res) =>{
+  const data = {
+    status : "200",
+    data : "baju",
+  }
+  res.status(200).json({
+    status: {
+      code: 200,
+      message: "Success"
+    },
+    data: data
+  })
+})
 
 const port = 8080
 app.listen(port, ()=>{
   console.log(
-    `listening on port https://localhost${port}`
+    `listening on port http://localhost:${port}`
     )
-})
+});
 
